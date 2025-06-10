dd4c950f88169d132f05860667e6f3955835578c
Shashank Singh <151665080+sksingh2005@users.noreply.github.com>
2025-06-11 02:18:28 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,5 @@ import { router } from './routes';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
     <RouterProvider router={router} />
-  </StrictMode>
 );
